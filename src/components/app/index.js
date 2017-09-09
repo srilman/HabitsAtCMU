50d3b0d5c0301fa79bf12b85b121e3b831a48095
Leon Chang <99leonchang@gmail.com>
2017-09-09 01:31:22 -0400

    Basic authentication

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -7,6 +7,8 @@ import { teal400 } from 'material-ui/styles/colors';
 import AppBar from 'material-ui/AppBar';
 import MenuItem from 'material-ui/MenuItem';
 import Drawer from 'material-ui/Drawer';
+import RaisedButton from 'material-ui/RaisedButton';
+import { auth, provider } from '../../firebase';
 
 const muiTheme = getMuiTheme({
     palette: {
@@ -19,8 +21,12 @@ export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            user: null,
             open: false,
         };
+
+        this.login = this.login.bind(this);
+        this.logout = this.logout.bind(this);
     }
 
     handleToggle = () => this.setState({open: !this.state.open});
@@ -28,21 +34,57 @@ export default class App extends React.Component {
     render() {
         return (
             <MuiThemeProvider muiTheme={muiTheme}>
-                <AppBar
-                    title="Title"
-                    onLeftIconButtonTouchTap={this.handleToggle}
-                />
-                <Drawer
-                    docked={false}
-                    width={300}
-                    open={this.state.open}
-                    onRequestChange={(open) => this.setState({open})}
-                >
-                    <MenuItem>Menu Item</MenuItem>
-                    <MenuItem>Menu Item 2</MenuItem>
-                </Drawer>
+                <div>
+                    <AppBar
+                        title="Title"
+                        onLeftIconButtonTouchTap={this.handleToggle}
+                    />
+                    <Drawer
+                        docked={false}
+                        width={300}
+                        open={this.state.open}
+                        onRequestChange={(open) => this.setState({open})}
+                    >
+                        <MenuItem>Menu Item</MenuItem>
+                        <MenuItem>Menu Item 2</MenuItem>
+                    </Drawer>
+                    <div className="wrapper">
+                        {this.state.user ?
+                            <RaisedButton onClick={this.logout}>Log Out</RaisedButton>
+                            :
+                            <RaisedButton onClick={this.login}>Log In</RaisedButton>
+                        }
+                    </div>
+                </div>
             </MuiThemeProvider>
         );
+    }
+
+    componentDidMount() {
+        auth.onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ user });
+            }
+        });
+    }
+
+    login() {
+        auth.signInWithPopup(provider)
+            .then((result) => {
+                const user = result.user;
+                this.setState({
+                    user,
+                });
+            });
+    }
+
+    logout() {
+        auth.signOut()
+            .then(() => {
+                this.setState({
+                    user:null,
+                });
+            });
     }
 
 }
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+const config = {
+    apiKey: 'AIzaSyBwGyvegYm9pdmutNFuLbUM2r2K74fDyDQ',
+    authDomain: 'habitatcmu-f7597.firebaseapp.com',
+    databaseURL: 'https://habitatcmu-f7597.firebaseio.com',
+    projectId: 'habitatcmu-f7597',
+    storageBucket: 'habitatcmu-f7597.appspot.com',
+    messagingSenderId: '407528962507',
+};
+firebase.initializeApp(config);
+export const provider = new firebase.auth.GoogleAuthProvider();
+export const auth = firebase.auth();
+export default firebase;
